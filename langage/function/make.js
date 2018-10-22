9d6d4814d442bcc62c70266945f515c5823e1f1e
David Gabison <dhg@outlook..fr>
2018-10-22 16:53:24 +0200

    utilisation de mkdirp pour creer les routes de sorties des makefiles

--- langage/function/make.js
+++ langage/function/make.js
@@ -1,5 +1,10 @@
 const pug = require('pug');
 const fs = require('fs');
+const mkdirp= require('mkdirp');
+
+String.prototype.toCapitalisationCase = function toCapitalisationCase() {
+  return this.charAt(0).toUpperCase() + this.substring(1);
+};
 
 const max = 5;
 
@@ -12,14 +17,16 @@ const th = [
 ];
 
 const list = [
-  'nom'
+  'nom',
+  'pos'
 ];
 
 list.forEach(function(name) {
+  mkdirp.sync('out/' + name);
   let recto = pug.compileFile(name + '_recto.pug');
   let verso = pug.compileFile(name + '_verso.pug');
   for (let i = 1; i <= max; ++i) {
-    fs.writeFileSync('out/' + i + name + '_recto.html', recto({th:th[i-1], i:i, max: max}));
-    fs.writeFileSync('out/' + i + name + '_verso.html', verso({th:th[i-1], i:i, max: max}));
+    fs.writeFileSync('out/' + name + '/' + i + name + '_recto.html', recto({ th: th[i - 1], i: i, max: max }));
+    fs.writeFileSync('out/' + name + '/' + i + name + '_verso.html', verso({ th: th[i - 1], i: i, max: max }));
   }
 });
