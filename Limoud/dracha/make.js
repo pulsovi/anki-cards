37a2bc40cff31e99a3def7d117ce4a6975792a4e
Puls Sovi <pulsovi@outlook.com>
2018-12-05 21:48:45 +0100

    ajout de la carte limoud:dracha::passouk

---
+++ Limoud/dracha/make.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+const pug = require('pug');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+
+const list = [
+  'Passouk',
+];
+
+mkdirp.sync('out');
+list.forEach(function(name) {
+  fs.writeFileSync('out/' + name + '_recto.html', pug.renderFile(name + '_recto.pug'));
+  fs.writeFileSync('out/' + name + '_verso.html', pug.renderFile(name + '_verso.pug'));
+});
