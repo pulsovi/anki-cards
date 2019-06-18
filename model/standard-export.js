974c15ee08193ac59988cf70bd43a170bcfa355d
Puls Sovi <pulsovi@outlook.com>
2019-06-18 17:00:04 +0200

    feat: added standard export module

---
+++ model/standard-export.js
@@ -0,0 +1,37 @@
+//jshint esversion:6
+const fs = require('fs');
+const path = require('path');
+const pug = require('pug');
+
+function make(
+  dirname,
+  cards = ['Card1'],
+  css = path.resolve(__dirname, 'commons.css'),
+  fields = [
+    '_recto',
+    '_verso'
+  ]
+) {
+  var outputList = [];
+
+  cards.forEach(function(name) {
+    fields.forEach(function(field) {
+      var pugFile = path.resolve(dirname, name + field + '.pug');
+      outputList.push({
+        content: pug.renderFile(pugFile),
+        name: 'out/' + name + field + '.html',
+        pugFile,
+      });
+    });
+  });
+
+  outputList.push({
+    content: fs.readFileSync(css, { encoding: 'utf8' }),
+    name: 'out/style.css',
+    pugFile: css,
+  });
+
+  return outputList;
+}
+
+module.exports = make;
