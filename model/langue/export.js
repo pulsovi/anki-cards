4dce7c67ed6a15ce7cc6f1468b28f53dd9a3474f
Puls Sovi <pulsovi@outlook.com>
2019-06-30 13:11:54 +0200

    feat: added langue cards models

---
+++ model/langue/export.js
@@ -0,0 +1,50 @@
+//jshint esversion:6
+const fs = require('fs');
+const path = require('path');
+const pug = require('pug');
+
+const langTo = {
+  "am": ["fr", "he"],
+  "en": ["fr"],
+  "fr": ["am", "en", "he"],
+  "he": ["am", "fr"],
+};
+
+const __ = {
+  "am": "Arameen",
+  "en": "Anglais",
+  "fr": "Francais",
+  "he": "Hebreu",
+};
+
+const outputList = [];
+const model = {
+  "recto": pug.compileFile(path.resolve(__dirname, 'langue_recto.pug')),
+  "verso": pug.compileFile(path.resolve(__dirname, 'langue_verso.pug')),
+};
+
+String.prototype.toCapitalisationCase = function toCapitalisationCase() {
+  return this.charAt(0).toUpperCase() + this.substring(1);
+};
+
+Object.keys(langTo).forEach(function(from) {
+  langTo[from].forEach(function(to) {
+    Object.keys(model).forEach(function(face) {
+      outputList.push({
+        content: model[face]({ from, to, fullFrom: __[from], fullTo: __[to] }),
+        name: 'out/' + [from, to, face].join('_') + '.html',
+        pugFile: path.resolve(__dirname, 'langue_' + face + '.pug'),
+      });
+    });
+  });
+});
+
+const css = path.resolve(__dirname, '../commons.css');
+outputList.push({
+  actualPath: css,
+  content: fs.readFileSync(css, { encoding: 'utf8' }),
+  name: 'out/style.css',
+  pugFile: css,
+});
+
+module.exports = outputList;
