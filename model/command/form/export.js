d1a5fe03830758118391b30627583a4c50345a98
Puls Sovi <pulsovi@outlook.com>
2019-07-10 12:21:32 +0200

    fix: use the standard export for command::form

--- model/command/form/export.js
+++ model/command/form/export.js
@@ -1,34 +1,4 @@
-//jshint esversion:6
-const fs = require('fs');
-const path = require('path');
-const pug = require('pug');
-
-const cards = [
+module.exports = require('../../standard-export')(__dirname, [
   'Form',
   'Usage',
-];
-
-const fields = [
-  '_recto',
-  '_verso'
-];
-
-var outputList = [];
-cards.forEach(function(name) {
-  fields.forEach(function(field) {
-    var outfile = {};
-    var pugFile = path.resolve(__dirname, name + field + '.pug');
-    outfile.pugFile = pugFile;
-    outfile.name = 'out/' + name + field + '.html';
-    outfile.content = pug.renderFile(pugFile);
-    outputList.push(outfile);
-  });
-});
-
-outputList.push({
-  name: 'out/style.css',
-  pugFile: path.resolve(__dirname, '../../commons.css'),
-  content: fs.readFileSync(path.resolve(__dirname, '../../commons.css'), { encoding: 'utf8' })
-});
-
-module.exports = outputList;
+]);
