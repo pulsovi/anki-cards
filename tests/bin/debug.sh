ffae269252120f8dabdeea0c616d7654654fcff3
David GABISON <david.gabison@outlook.com>
2020-06-21 11:36:58 +0200

    doc: test/bin/debug is now documented and eslint OK

--- tests/bin/debug.sh
+++ tests/bin/debug.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+# Usage: debug <cid>
+# <cid>: card id in anki
+
 export ANKI_PUG_ROOT=`realpath "$(dirname "$0")/../.."`
 node "$(dirname "$0")/../src/debug/index.js" "$*"
--- tests/src/debug/index.js
+++ tests/src/debug/index.js
@@ -1,22 +1,20 @@
-//jshint esversion:8
 const FixtureManager = require('./fixture-manager');
+
+function helpMessage() {
+  console.log('usage: debug <cid>\n\ncid: card id in Anki');
+  FixtureManager.close();
+}
+
+async function main() {
+  if (process.argv.length < 3 || ['', '--help', '-h'].includes(process.argv[2])) return helpMessage();
+  const cid = process.argv[2];
+  const manager = new FixtureManager(cid);
+  await manager.show();
+  return 0;
+}
 
 // main
 main()
-  .catch(e => {
-    console.log('Main error:', e);
+  .catch((error) => {
+    console.log('Error in main function:', error);
   });
-
-async function main() {
-  if (process.argv.length < 3 || ["", "--help", "-h"].includes(process.argv[2])) return helpMessage();
-  var cid = process.argv[2];
-  var manager = new FixtureManager(cid);
-  await manager.show();
-}
-
-function helpMessage() {
-  console.log(`usage: debug <cid>
-
-    cid: card id in Anki`);
-  FixtureManager.close();
-}
