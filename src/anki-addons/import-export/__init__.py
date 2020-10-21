f34657de483293e728899ea02389bf5276296882
David GABISON <david.gabison@outlook.com>
2020-10-21 09:14:16 +0200

    refactor: clean and reorder function names

--- src/anki-addons/import-export/__init__.py
+++ src/anki-addons/import-export/__init__.py
@@ -10,9 +10,22 @@ from anki.hooks import addHook, remHook
 ROOT_FOLDER = "E:\\dev\\03 - Anki\\anki-cards\\model"
 
 
-def export_models():
-    for model in mw.col.models.all():
-        export_model(model)
+def add_button(title, action):
+    button = qt.QAction(title, mw)
+    button.triggered.connect(action)
+    mw.form.menuTools.addAction(button)
+
+
+def add_template(model, name, folder):
+    model_manager = mw.col.models
+    template = model_manager.newTemplate(name)
+    file = os.path.join(folder, name)
+    recto = read_file(file + "_recto.html")  # recto
+    verso = read_file(file + "_verso.html")  # verso
+    template["qfmt"] = recto
+    template["afmt"] = verso
+    model_manager.addTemplate(model, template)
+    return "\n\tadded " + name
 
 
 def export_model(model):
@@ -24,21 +37,13 @@ def export_model(model):
         print_card(folder, card)
 
 
-def print_card(folder, card):
-    name = card["name"]
-    recto = card["qfmt"]
-    verso = card["afmt"]
-    write_file(os.path.join(folder, name + "_recto.html"), recto)
-    write_file(os.path.join(folder, name + "_verso.html"), verso)
+def export_models():
+    for model in mw.col.models.all():
+        export_model(model)
 
 
-def import_models():
-    ShowCardCount()
-    message = ""
-    for key in mw.col.models.models:
-        message += import_model(mw.col.models.models[key])
-    mw.col.models.flush()
-    ShowCardCount(message)
+def get_model_folder(name):
+    return os.path.join(ROOT_FOLDER, *(name.split("::")), "out")
 
 
 def import_model(model):
@@ -64,20 +69,50 @@ def import_model(model):
     return "\n" + name + " :" + message
 
 
-def add_template(model, name, folder):
-    model_manager = mw.col.models
-    template = model_manager.newTemplate(name)
-    file = os.path.join(folder, name)
-    recto = read_file(file + "_recto.html")  # recto
-    verso = read_file(file + "_verso.html")  # verso
-    template["qfmt"] = recto
-    template["afmt"] = verso
-    model_manager.addTemplate(model, template)
-    return "\n\tadded " + name
+def import_models():
+    show_card_count()
+    message = ""
+    for key in mw.col.models.models:
+        message += import_model(mw.col.models.models[key])
+    mw.col.models.flush()
+    show_card_count(message)
 
 
-def get_model_folder(name):
-    return os.path.join(ROOT_FOLDER, *(name.split("::")), "out")
+def list_new_cards(model):
+    files = list(os.walk(get_model_folder(model["name"])))[0][2]
+    old_cards = [i["name"] for i in model["tmpls"]]
+    new_cards = [i.replace('_recto.html', '') for i in files if
+                 i.endswith('_recto.html') and
+                 i.replace('recto', 'verso') in files and
+                 i.replace('_recto.html', '') not in old_cards]
+    return new_cards
+
+
+def normalize(data):
+    return b'\n'.join(
+        bytes(i, 'utf8') for i in data.split('\n')
+    )
+
+
+def on_sync(state):
+    try:
+        if(state == 'findMedia'):
+            print('anki:ready', end='', flush=True)
+            remHook('sync', on_sync)
+    except:
+        pass
+
+
+def print_card(folder, card):
+    name = card["name"]
+    recto = card["qfmt"]
+    verso = card["afmt"]
+    write_file(os.path.join(folder, name + "_recto.html"), recto)
+    write_file(os.path.join(folder, name + "_verso.html"), verso)
+
+
+def print_css(folder, data):
+    write_file(os.path.join(folder, "style.css"), data)
 
 
 def read_card(folder, card):
@@ -95,35 +130,8 @@ def read_card(folder, card):
     return message
 
 
-def list_new_cards(model):
-    files = list(os.walk(get_model_folder(model["name"])))[0][2]
-    old_cards = [i["name"] for i in model["tmpls"]]
-    new_cards = [i.replace('_recto.html', '') for i in files if
-                 i.endswith('_recto.html') and
-                 i.replace('recto', 'verso') in files and
-                 i.replace('_recto.html', '') not in old_cards]
-    return new_cards
-
-
-def print_css(folder, data):
-    write_file(os.path.join(folder, "style.css"), data)
-
-
 def read_css(folder):
     return read_file(os.path.join(folder, "style.css"))
-
-
-def write_file(filename, data):
-    pathlib.Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
-    f = open(filename, 'wb')
-    f.write(normalize(data if data else ""))
-    f.close()
-
-
-def normalize(data):
-    return b'\n'.join(
-        bytes(i, 'utf8') for i in data.split('\n')
-    )
 
 
 def read_file(filename):
@@ -135,31 +143,18 @@ def read_file(filename):
     return data
 
 
-def ShowCardCount(message=""):
+def show_card_count(message=""):
     cardCount = mw.col.cardCount()
     showInfo(("Card count: %d" % cardCount) + message)
 
 
-def onSync(state):
-    try:
-        if(state == 'findMedia'):
-            print('anki:ready', end='', flush=True)
-            remHook('sync', onSync)
-    except:
-        pass
+def write_file(filename, data):
+    pathlib.Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
+    f = open(filename, 'wb')
+    f.write(normalize(data if data else ""))
+    f.close()
 
 
-def AddBouton(title, action):
-    button = qt.QAction(title, mw)
-    button.triggered.connect(action)
-    mw.form.menuTools.addAction(button)
-
-
-def Test():
-    models = mw.col.models.all()
-    showInfo(str(len(models)) + "\n" + str(models[0]))
-
-AddBouton("Exporter les models", export_models)
-AddBouton("Importer les models", import_models)
-AddBouton("Test", Test)
-addHook('sync', onSync)
+add_button("Exporter les models", export_models)
+add_button("Importer les models", import_models)
+addHook('sync', on_sync)
