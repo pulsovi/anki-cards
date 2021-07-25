18a75513288c07c9d875a3a020391587d613ecca
David GABISON <david.gabison@outlook.com>
2021-07-25 22:30:29 +0200

    refactor: more verbose error messages

--- bin/diff.sh
+++ bin/diff.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-node --trace-warnings `dirname $0`/../src/diff/anki-pug-diff.js
+node --trace-warnings --async-stack-traces --unhandled-rejections=strict "$(dirname "$0")/../src/diff/anki-pug-diff.js" "$*"
