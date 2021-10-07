63ba873c3e2b7e69e32b0ab9779ac267113a8fe5
David GABISON <david.gabison@outlook.com>
2021-10-07 15:29:29 +0200

    refactor: clean

--- src/diff/file_manager.js
+++ src/diff/file_manager.js
@@ -1,13 +1,10 @@
-// jshint esversion:8
 const childProcess = require('child_process');
 const fs = require('fs');
 const path = require('path');
-const { deprecate } = require('util');
+
+const log = require('debug')('anki:file-manager');
 
 const FileManager = {
-  fileNormalized(filename) {
-    return this.normalizeEnding(fs.readFileSync(filename, 'utf8'));
-  },
   normalizeEnding(string) {
     let result = string;
 
@@ -15,26 +12,22 @@ const FileManager = {
     return result.replace(/\r\n/gu, '\n').replace(/\r/gu, '\n').replace(/\n*$/u, '\n');
   },
   open(...files) {
-    files.forEach(viewFile);
+    files.forEach(openFile);
   },
   waitOnce(...files) {
     return new Promise(resolve => {
       const watchers = files.map(file => {
-        if (typeof file !== 'string') {
-          console.error(`${file} is not a string`);
-          throw new TypeError('files MUST be of type string');
-        }
         const basename = path.basename(file);
         const directory = path.dirname(file);
         const watcher = fs.watch(directory, (event, filename) => {
           if (filename !== basename) return;
-          console.info(event, filename);
-          watchers.forEach(wtchr => wtchr.close());
+          log(event, filename);
+          watchers.forEach(watcherItem => watcherItem.close());
           resolve(file);
         });
 
         console.info('watch file:', file);
-        viewFile(file);
+        openFile(file);
         return watcher;
       });
     });
@@ -45,12 +38,8 @@ function run(command) {
   childProcess.spawn(command, { detached: true, shell: true, stdio: 'ignore' }).unref();
 }
 
-function viewFile(filename) {
+function openFile(filename) {
   run(`"C:\\Program Files\\Sublime Text 3\\sublime_text.exe" "${filename}"`);
 }
 
-FileManager.fileNormalized = deprecate(
-  FileManager.fileNormalized,
-  'fileNormalized() is deprecated, please normalize the original file instead'
-);
 module.exports = FileManager;
