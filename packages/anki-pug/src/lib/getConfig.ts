95f61705a117020dbd23c1c170f76030ed2d5e18
David GABISON <david.gabison@outlook.com>
2022-07-13 22:24:27 +0200

    feat: improve config error log

--- packages/anki-pug/src/lib/getConfig.ts
+++ packages/anki-pug/src/lib/getConfig.ts
@@ -21,6 +21,40 @@ const defaults = {
   modelsPath: './model',
 };
 
+class ValidationError extends Error {
+  public constructor (error: Error, results: {
+    _: unknown;
+    __: unknown;
+    configs: string[] | undefined;
+    params: AnkiPugConfig;
+  }) {
+    const { _, __, configs, params } = results;
+    let message = 'There is an error in one of your config files, \n';
+
+    if (Array.isArray(configs) && configs.length) {
+      message += 'config files found:\n  - ';
+      message += configs.join('\n  - ');
+    } else
+      message += `No config file found. Think create file named ".${appName}rc".`;
+    message += '\n';
+
+    if ((Array.isArray(__) && __.length) || (Array.isArray(_) && _.length)) {
+      message += 'argv:\n';
+      if (Array.isArray(__) && __.length)
+        message += `\t--: ${__.join(', ')}\n`;
+      if (Array.isArray(_) && _.length)
+        message += `\t_: ${_.join(', ')}\n`;
+    }
+
+    message += 'parsed config :';
+    message += JSON.stringify(params, null, 2);
+    console.info(message);
+
+    super(error.message);
+    this.name = 'ValidationError';
+  }
+}
+
 export default function getConfig (
   argv: object | null = null
 ): AnkiPugConfig {
@@ -28,7 +62,7 @@ export default function getConfig (
   const { '--': __, _, config, configs, ...params } = rcResult;
   const { error } = schema.validate(params);
 
-  if (error) throw logError(error, { _, __, configs, params });
+  if (error) throw new ValidationError(error, { _, __, configs, params });
 
   if (config) params.configPath = config;
   return params;
@@ -36,37 +70,4 @@ export default function getConfig (
 
 function loadYaml (content: string): object {
   return load(content) as object;
-}
-
-function logError (error: Error, results: {
-  _: unknown;
-  __: unknown;
-  configs: string[] | undefined;
-  params: AnkiPugConfig;
-}): Error {
-  const { _, __, configs, params } = results;
-  let message = 'There is an error in one of your config files, \n';
-
-  if (Array.isArray(configs) && configs.length) {
-    message += 'config files found:\n\t- ';
-    message += configs.join('\n\t- ');
-  } else
-    message += `No config file found. Think create file named ".${appName}rc".`;
-  message += '\n';
-
-  if ((Array.isArray(__) && __.length) || (Array.isArray(_) && _.length)) {
-    message += 'argv:\n';
-    if (Array.isArray(__) && __.length)
-      message += `\t--: ${__.join(', ')}\n`;
-    if (Array.isArray(_) && _.length)
-      message += `\t_: ${_.join(', ')}\n`;
-  }
-
-  message += 'parsed config :';
-  message += JSON.stringify(params, null, 4);
-  message += '\n';
-
-  message += error.message;
-  error.message = message;
-  return error;
 }
